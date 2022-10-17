2734ddd369332b6978fab7b40d5a308c52942f81
BrightCoderofRishtan <muhammadjongulomov94@gmail.com>
2022-10-17 09:22:44 -0700

    Recursive fun

--- v28/v28.dart
+++ v28/v28.dart
@@ -1,9 +1,20 @@
-// Reversive function
-void main() {
-  print(fct(5));
-}
-// find a! factorial 
-int fct(int a) {
-  if (a == 1) return 1;
-  return fct(a - 1) * a;
+// // Reversive function
+// void main() {
+//   print(fct(5));
+// }
+// // find a! factorial  1-way
+// int fct(int a) {
+//   if (a == 1) return 1;
+//   return fct(a - 1) * a;
+// }
+
+// find n! foctorial 2-way
+
+void main(List<String> args) {
+  int n = 5;
+  int fct = 1;
+  for (int i = 1; i <= n; i++) {
+    fct = fct * i;
+  }
+  print(fct);
 }
